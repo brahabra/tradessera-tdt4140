45dcea08e4c6d06049f77eb8cfe5cda509dc5ad9
Trygve Aanestad <trygvaan@stud.ntnu.no>
2022-02-15 17:46:45 +0100

    Added functional input fields on the login page
    Related to #1
    
    Co-authored-by: Anna Johanne Holden Jacobsen <ajjacobs@ntnu.no>

--- basic/src/main/js/app.js
+++ basic/src/main/js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // tag::vars[]
 const React = require('react'); // <1>
 const ReactDOM = require('react-dom'); // <2>
@@ -12,6 +13,7 @@ class App extends React.Component { // <1>
 	constructor(props) {
 		super(props);
 		this.state = {users: []};
+		
 	}
 
 	componentDidMount() { // <2>
@@ -20,25 +22,13 @@ class App extends React.Component { // <1>
 		});
 	}
 
-	loggedIn() {
-		alert('You are now logged in!');
-	}
 
-	registered() {
-		alert('You are now registered!');
-	}
 
 	render() { // <3>
 		return (
 			<div>
 				<UserList users={this.state.users}/>
 				<Login/>
-				<button onClick={this.loggedIn}>
-  				LOGIN
-				</button>
-				<button onClick={this.registered}>
-  				REGISTER
-				</button>
 			</div>
 		)
 	}
@@ -78,25 +68,122 @@ class UserList extends React.Component{
 }
 
 class Login extends React.Component{
+    
+	constructor(props) {
+		super(props);
+		this.state = {name: '', mail: '', pass: ''};
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChangeName = this.handleChangeName.bind(this);
+		this.handleChangeMail = this.handleChangeMail.bind(this);
+		this.handleChangePass = this.handleChangePass.bind(this);
+	}
+
+	handleChangeName(event) {
+		let {value} = event.target;
+		this.setState({name: value});
+	}
+	handleChangeMail(event) {
+		let {value} = event.target;
+		this.setState({mail: value});
+	}
+	handleChangePass(event) {
+		let {value} = event.target;
+		this.setState({pass: value});
+	}
+	
+	handleSubmit(event) {
+		alert('A name was submitted: ' + this.state.name + "\nA mail: " + this.state.mail + "\nAnd a password: " + this.state.pass);
+		event.preventDefault();
+	}
+/*
+	loggedIn(){
+		alert('A name was submitted: ' + this.state.inputName);
+	}
+*/
+	registered() {
+		alert('You are now registered!');
+	}
+    
 	render() {
 		return (
 			<div>
-				<div>
-				<label>Username:</label>
-				<input/>
-				</div>
-				<div>
-				<label>Email:</label>
-				<input/>
-				</div>
-				<div>
-				<label>Password:</label>
-				<input/>
-				</div>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Username:
+						<input type="text" value={this.state.name} placeholder="Enter Username"
+						 onChange={event => this.handleChangeName(event)} />
+					</label>
+					<br></br>
+					<label>
+						Email:
+						<input type="text" value={this.state.mail} placeholder="Enter Email"
+						 onChange={event => this.handleChangeMail(event)} />
+					</label>
+					<br></br>
+					<label>
+						Password:
+						<input type="text" value={this.state.pass} placeholder="Enter Password"
+						 onChange={event => this.handleChangePass(event)} />
+					</label>
+					<br></br>
+					<input type="submit" value="Login" />
+				</form>
+
+				<button onClick={this.registered}>
+  				REGISTER
+				</button>
 			</div>
 		)
 	}
 }
+
+class CreateDialog extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const newUser = {};
+		this.props.attributes.forEach(attribute => {
+			newUser[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+		});
+		this.props.onCreate(newUser);
+
+		// clear out the dialog's inputs
+		this.props.attributes.forEach(attribute => {
+			ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+		});
+
+		// Navigate away from the dialog to hide it.
+		window.location = "#";
+	}
+
+	render() {
+		return (
+			<div>
+				<a href="#createUser">Create</a>
+
+				<div id="createUser" className="modalDialog">
+					<div>
+						<a href="#" title="Close" className="close">X</a>
+
+						<h2>Create new User</h2>
+
+						<form>
+							{inputs}
+							<button onClick={this.handleSubmit}>logging In</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+}
+
 
 // tag::render[]
 ReactDOM.render(
