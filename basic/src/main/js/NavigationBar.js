f7e2f747ac699509bae4635bd7894d3869e07785
Julie Lundberg Suter <julielsu@stud.ntnu.no>
2022-02-24 13:57:56 +0100

    Initial commit for adding logo
    Started the process to add logo in the navbar
    
    Related to #17

--- basic/src/main/js/NavigationBar.js
+++ basic/src/main/js/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Navbar, Nav, Container} from 'react-bootstrap';
+import Logo from '../../../Logo.png';
 
 
 
@@ -9,11 +10,21 @@ export default class NavigatonBar extends React.Component {
         return (
             <Navbar bg="dark" variant="dark">
                 <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img 
+                    alt=""
+                    src={Logo}
+                    width="30"
+                    height="30"
+                    /> {' '} Tradessera
+                </Navbar.Brand>
                 <Nav className="me-auto">
                 <Nav.Link href="..">Home</Nav.Link>
                 <Nav.Link href="register">Register</Nav.Link>
                 <Nav.Link href="login">Login</Nav.Link>
+                <Nav.Link href="users">Users</Nav.Link>
+                <Nav.Link href="posts">Posts</Nav.Link>
+                <Nav.Link href="createPost">CreatePost</Nav.Link>
                 </Nav>
                 </Container>
             </Navbar>
